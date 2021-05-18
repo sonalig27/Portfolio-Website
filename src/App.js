4921d984aef0e279b5d36e3edca9c7a42a3aa02d
sonalig27 <ganatra.sonali@gmail.com>
2021-05-17 22:09:43 -0400

    Header Component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,72 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-
+import Header from "./components/Header";
+import { Fragment } from "react";
+import Particles from "react-particles-js";
 function App() {
-  return <Navbar />;
+  return (
+    <Fragment>
+      <Particles
+        className="particles-canvas"
+        params={{
+          fpsLimit: 60,
+          particles: {
+            color: {
+              value: [
+                "#d81ec8",
+                "#EC6711",
+                "#F2F542",
+                "#42B9F5",
+                "#3aF030",
+                "#741AEB",
+              ],
+            },
+            links: {
+              color: {
+                value: ["#d81ec8"],
+              },
+              distance: 150,
+              opacity: 0.5,
+              enable: true,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.8,
+            },
+            shape: {
+              type: "star",
+            },
+            size: {
+              random: true,
+              value: 4,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Frontend Developer</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Responsive Design",
+            "App Development",
+            "Testing/Debugging",
+            "Product and Maintenance",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="header-btn">
+          Contact Me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
